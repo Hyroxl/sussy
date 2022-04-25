8951850bfbdad351a92cd190e73e2e13605f0036
Tyler Paul <98190955+TylerPaul926@users.noreply.github.com>
2022-04-25 13:52:34 -0400

    Create Amogus.java

---
+++ Amogus.java
@@ -0,0 +1,8 @@
+public class Main {
+    static void amogus (String name) {
+    System.out.println(name +' is sus"):
+    }
+    public static void main(String[] args) f
+    amogus ("The imposter") ;
+    }
+}
